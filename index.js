989a61293f1819ac058d11adc2275c7d789b4c18
Badru <badruakfm@gmail.com>
2021-08-22 17:03:42 +0700

    cors

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 require('dotenv').config()
+const cors = require('cors')
 
 const notes = require('./routes/notes')
 
 const app = express();
 
 app.use(bodyParser.json())
+app.use(cors())
 
 mongoose.connect(process.env.MONGODB_URL, {
         useNewUrlParser: true,
